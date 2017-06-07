2afe0ce59fb8dd191696e5083874d6cbb8cecf6b
Arunas <arunijus@gmail.com>
2017-06-07 16:36:01 +0300

    product and variation

--- app/controllers/importer_controller.rb
+++ app/controllers/importer_controller.rb
@@ -3,10 +3,11 @@ class ImporterController < ApplicationController
   require 'net/http'
 
   def index
-    source = "http://live.icecat.biz/api/?shopname=openIcecat-live&lang=lt&content=&icecat_id=17496464"
-    resp = Net::HTTP.get_response(URI.parse(source))
-    @data = resp.body
+    # source = "http://live.icecat.biz/api/?shopname=openIcecat-live&lang=lt&content=&icecat_id=17496464"
+    # resp = Net::HTTP.get_response(URI.parse(source))
+    # @data = resp.body
 
-    @product = Product.create()
+    @product = Product.create(:uuid => SecureRandom.uuid)
+    @variation = Variation.create(:product => @product, :position => 1, :uuid => SecureRandom.uuid)
   end
 end
