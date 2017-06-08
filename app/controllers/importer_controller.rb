e530f03faeacf1f825da9532b992337f92f5bffa
Arunas <arunijus@gmail.com>
2017-06-08 20:42:00 +0300

    photos uploaded

--- app/controllers/importer_controller.rb
+++ app/controllers/importer_controller.rb
@@ -5,70 +5,80 @@ class ImporterController < ApplicationController
   LOCALES = ['en', 'lt', 'lv', 'et']
 
   def index
+    productIds = ["17496464"]
+
     # # Read index file for product ids
     # xml_file = File.read(Rails.root + 'lib/docs/files.test.index.xml')
     #
     # doc = Nokogiri::XML.parse(xml_file)
     #
     # count = 0;
-    # productIds = []
     # doc.xpath('//file').each do |file|
     #   productIds << file["Product_ID"]
     #   count = count + 1
     # end
-    #
-    @supplier = Supplier.find_by name: "Icecat"
-    @shop = Shop.find_by name: "Elfas"
-    @supplierCategory = SupplierCategory.find_by name: "Icecat"
-    @category = Category.find_by name: "Icecat"
 
-    @pj = ImporterController.icecat_map(17496464)
+    productIds.each do |productId|
+      @supplier = Supplier.find_by name: "Icecat"
+      @shop = Shop.find_by name: "Elfas"
+      @supplierCategory = SupplierCategory.find_by name: "Icecat"
+      @category = Category.find_by name: "Icecat"
 
-    supplier_item = SupplierItem.find_by foreign_id: @pj["icecat_id"]
-
-    unless supplier_item
-      @productFamily = ProductFamily.find_by foreign_id: @pj["product_family_id"]
-
-      unless @productFamily
-        @productFamily = ProductFamily.create(:name => @pj["product_family_name"], :foreign_id => @pj["product_family_id"], :is_approved => 0)
-      end
-
-      @product = Product.create(:uuid => SecureRandom.uuid, :family => @productFamily)
-      @variation = Variation.create(:product => @product, :position => 1, :uuid => SecureRandom.uuid)
-      @supplierItem = SupplierItem.create(:variation => @variation, :supplier => @supplier, :supplier_category => @supplierCategory, :foreign_id => @pj["icecat_id"], :payload => @pj["data"])
-
-      @pj["product_gtins"].each do |gtin|
-        VariationGtin.create(:supplier => @supplier, :variation => @variation, :value => gtin)
-      end
-
-      Assortment.create(:product => @product, :shop => @shop, :is_active => 1, :status => 'Approved')
-
+      @pj = ImporterController.icecat_map(productId)
       @data = @pj["data"]
 
-      @pj["attributes"].each do |a|
-        supplier_attribute = SupplierAttribute.find_by foreign_id: a["attribute_id"]
+      supplier_item = SupplierItem.find_by foreign_id: @pj["icecat_id"]
 
-        unless supplier_attribute
-          @attribute = Attribute.create(:uuid => SecureRandom.uuid, :suffix => a["feature_measure"])
-          AttributeTranslation.create(:name => a["attribute_name"], :locale => "lt", :attr => @attribute)
-          @attributeValue = AttributeValue.create(:att => @attribute)
+      unless supplier_item
+        @productFamily = ProductFamily.find_by foreign_id: @pj["product_family_id"]
 
-          ProductAttributeValue.create(:product => @product,
-                                                  :att => @attribute,
-                                                  :attribute_value => @attributeValue,
-                                                  :supplier => @supplier)
-
-          AttributeValueTranslation.create(:attr_value => a["attribute_value"], :value_hash => a["attribute_value"].hash, :locale => "lt", :attribute_value => @attributeValue)
-
-          @supplierAttribute = SupplierAttribute.create(:att => @attribute, :supplier => @supplier, :foreign_id => a["attribute_id"])
-          SupplierAttributeTranslation.create(:name => a["attribute_name"], :locale => "lt", :supplier_attribute => @supplierAttribute)
-
-          @supplierItemAttributeValue = SupplierItemAttributeValue.create(:supplier_item => @supplierItem, :supplier_attribute => @supplierAttribute, :has_duplicates => 0)
-          SupplierItemAttributeValueTranslation.create(:supplier_item_attribute_value => @supplierItemAttributeValue, :value => a["attribute_value"], :transformed_value => a["attribute_value"], :last_checked_value => a["attribute_value"], :locale => "lt")
+        unless @productFamily
+          @productFamily = ProductFamily.create(:name => @pj["product_family_name"], :foreign_id => @pj["product_family_id"], :is_approved => 0)
         end
-      end
 
-      ProductCategory.create(:product => @product, :category => @category)
+        @product = Product.create(:uuid => SecureRandom.uuid, :family => @productFamily)
+        @variation = Variation.create(:product => @product, :position => 1, :uuid => SecureRandom.uuid)
+        @supplierItem = SupplierItem.create(:variation => @variation, :supplier => @supplier, :supplier_category => @supplierCategory, :foreign_id => @pj["icecat_id"], :payload => @pj["data"])
+
+        @pj["product_gtins"].each do |gtin|
+          VariationGtin.create(:supplier => @supplier, :variation => @variation, :value => gtin)
+        end
+
+        ProductCategory.create(:product => @product, :category => @category)
+
+        Assortment.create(:product => @product, :shop => @shop, :is_active => 1, :status => 'Approved')
+
+        @pj["attributes"].each do |a|
+          supplier_attribute = SupplierAttribute.find_by foreign_id: a["attribute_id"]
+
+          unless supplier_attribute
+            @attribute = Attribute.create(:uuid => SecureRandom.uuid, :suffix => a["feature_measure"])
+            AttributeTranslation.create(:name => a["attribute_name"], :locale => "lt", :attr => @attribute)
+            @attributeValue = AttributeValue.create(:att => @attribute)
+
+            ProductAttributeValue.create(:product => @product,
+                                                    :att => @attribute,
+                                                    :attribute_value => @attributeValue,
+                                                    :supplier => @supplier)
+
+            AttributeValueTranslation.create(:attr_value => a["attribute_value"], :value_hash => a["attribute_value"].hash, :locale => "lt", :attribute_value => @attributeValue)
+
+            @supplierAttribute = SupplierAttribute.create(:att => @attribute, :supplier => @supplier, :foreign_id => a["attribute_id"])
+            SupplierAttributeTranslation.create(:name => a["attribute_name"], :locale => "lt", :supplier_attribute => @supplierAttribute)
+
+            @supplierItemAttributeValue = SupplierItemAttributeValue.create(:supplier_item => @supplierItem, :supplier_attribute => @supplierAttribute, :has_duplicates => 0)
+            SupplierItemAttributeValueTranslation.create(:supplier_item_attribute_value => @supplierItemAttributeValue, :value => a["attribute_value"], :transformed_value => a["attribute_value"], :last_checked_value => a["attribute_value"], :locale => "lt")
+          end
+        end
+
+        image_position = 0
+        @pj["images"].each do |image|
+            position = image["isMain"] ? 0 : image_position = image_position + 1
+            VariationPhoto.create(:variation => @variation, :source => "Icecat::HTML", :file => image["file"], :position => position, :size => image["size"], :width => image["width"], :height => image["height"], :status => "active")
+        end
+
+
+      end
     end
   end
 
@@ -98,24 +108,25 @@ class ImporterController < ApplicationController
         feature_name = feature["Feature"]["Name"]["Value"]
         feature_measure = feature["Feature"]["Measure"]["Signs"]["_"]
         feature_value = feature["Value"]
-        feats.push({"attribute_id"=>feature_id, "attribute_name"=>feature_name,
-          "attribute_value"=>feature_value, "attribute_sign"=>feature_measure})
+        feats.push({"attribute_id"=>feature_id, "attribute_name"=>feature_name, "attribute_value"=>feature_value, "attribute_sign"=>feature_measure})
       end
+    end
+
+    images = []
+    gallery = parsed_json["data"]["Gallery"]
+
+    gallery.each do |gallery_item|
+      image_file = gallery_item["Pic"]
+      image_size = gallery_item["Size"]
+      image_width = gallery_item["PicWidth"]
+      image_height = gallery_item["PicHeight"]
+      is_main = gallery_item["IsMain"] === "Y" ? true : false
+
+      images.push({"file" => image_file, "size" => image_size, "width" => image_width, "height" => image_height, "isMain" => is_main})
     end
 
     return {"data"=>data,"product_name"=>product_name, "icecat_id" => icecat_id, "product_brand"=>product_brand,"product_gtins"=>product_gtins,
           "product_category_id"=>product_category_id,"product_family_id"=> product_family_id,"product_description"=>product_description,
-        "attributes"=>feats, "product_family_name"=> product_family_name }
+        "attributes"=>feats, "product_family_name"=> product_family_name, "images" => images }
   end
-
 end
-
-
-
-
-
-#     @product_import = Product.create(:uuid => SecureRandom.uuid)
-#     @variation_import = Variation.create(:product => @product_import, :position => 1, :uuid => SecureRandom.uuid)
-#     @attribute_import = Attribute.create( :suffix => "m^3", :value_type => "cubic meters")
-#     @attribute_value_import = AttributeValue.create(:att => @attribute_import, :identifier => "sel")
-#     @attribute_translation_import = AttributeTranslation.create(:locale => "lt", :name => "vonios uždangalai")
